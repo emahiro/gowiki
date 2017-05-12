7984daafc525e05ce148f4cb9d89b0d693bc604b
ememhr <hiromichi.ema@dena.jp>
2017-05-12 16:27:45 +0900

    http handler 追加

--- app.go
+++ app.go
@@ -2,11 +2,20 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"io/ioutil"
+	"net/http"
 	"os"
+	"regexp"
 )
 
 var dataDir = "data/"
+
+// Template Caching
+var templates = template.Must(template.ParseFiles("views/view.html", "views/edit.html"))
+
+// validation
+var validPath = regexp.MustCompile("^/(view|edit|save)/([a-zA-Z0-9]+)$")
 
 // Page Title and Body
 type Page struct {
@@ -38,15 +47,54 @@ func loadPage(title string) (*Page, error) {
 	return page, nil
 }
 
+// HTML Rendering
+func renderTemplate(w http.ResponseWriter, tmpl string, page *Page) {
+	err := templates.ExecuteTemplate(w, tmpl+".html", page)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+// HTTP Handler
+func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		matched := validPath.FindStringSubmatch(r.URL.Path)
+		if matched != nil {
+			http.NotFound(w, r)
+			return
+		}
+		fn(w, r, matched[2])
+	}
+}
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "/view/FrontPage", http.StatusFound)
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
+	page, err := loadPage(title)
+	if err != nil {
+		fmt.Println(title + " is not found")
+		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+		return
+	}
+
+	renderTemplate(w, "view", page)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request, title string) {
+	page, err := loadPage(title)
+	if err != nil {
+		page = &Page{Title: title}
+	}
+
+	renderTemplate(w, "edit", page)
+}
+
 func main() {
-	p := &Page{Title: "test", Body: []byte("This is test")}
-	err := p.save()
-	if err != nil {
-		fmt.Println("Failed")
-	}
-	page, err := loadPage("test")
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Printf("Loading page data is %s", page.Body)
+	http.HandleFunc("/", rootHandler)
+	http.HandleFunc("/view/", makeHandler(viewHandler))
+	http.HandleFunc("/edit/", makeHandler(editHandler))
+	http.ListenAndServe(":8080", nil)
 }
